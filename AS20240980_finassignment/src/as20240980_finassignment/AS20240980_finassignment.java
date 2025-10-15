17d91cede731df1f32a826cb0cc9df4ad56c6874
asekakasundi <aseka.kasundi@gmail.com>
2025-10-15 16:55:52 +0530

    trial

--- AS20240980_finassignment/src/as20240980_finassignment/AS20240980_finassignment.java
+++ AS20240980_finassignment/src/as20240980_finassignment/AS20240980_finassignment.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package as20240980_finassignment;
 
-/**
- *
- * @author DELL
- */
 public class AS20240980_finassignment {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
